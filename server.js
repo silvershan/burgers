bd7899bc8c8c3b46968172ced7e2c3c46903342f
silvershan <44039173+silvershan@users.noreply.github.com>
2019-03-28 16:48:30 -0700

    Update server.js

--- server.js
+++ server.js
@@ -24,14 +24,6 @@ var routes = require("./controllers/burgers_controller.js");
 
 app.use(routes);
 
-// Timeout
-app.use(timeout(15000));
-app.use(haltOnTimedout);
-
-function haltOnTimedout(req, res, next) {
-    if (!req.timedout) next();
-}
-
 app.listen(PORT, function() {
     console.log("Server listening on: http://localhost:" + PORT);
 });
